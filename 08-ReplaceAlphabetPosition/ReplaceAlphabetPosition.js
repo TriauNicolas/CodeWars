c3ff222692dc72b30202d97617969c3197ceabaf
Nicolas <nicolas.triau@hetic.net>
2021-11-25 12:55:29 +0100

    Init ReplaceAlphabetPosition exercice

---
+++ 08-ReplaceAlphabetPosition/ReplaceAlphabetPosition.js
@@ -0,0 +1,14 @@
+function alphabetPosition(text) {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  let text_split = text.toUpperCase().split('');
+  let new_text = '';
+  
+  text_split.forEach((letter, indexText) => {
+    alphabet.find((alphabet, index) => {
+      if(letter === alphabet) {
+        new_text += index + 1 + ' ';
+      }
+    })
+  })
+  return new_text.trim();
+}
